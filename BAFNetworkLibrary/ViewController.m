ef5460f5a8b551ec9585a7b3516b75ace76401d1
beautilut <beautilut@gmail.com>
2016-07-13 18:05:05 +0800

    补全基本内容

--- BAFNetworkLibrary/ViewController.m
+++ BAFNetworkLibrary/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
 @interface ViewController ()
 
@@ -24,4 +25,113 @@
     // Dispose of any resources that can be recreated.
 }
 
+//GET 方法
+-(void)get
+{
+    AFHTTPSessionManager * manager = [AFHTTPSessionManager manager];
+    
+    [manager GET:@"" parameters:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+    }];
+    
+}
+
+//POST 方法
+-(void)post
+{
+    AFHTTPSessionManager * manager = [AFHTTPSessionManager manager];
+    
+    NSMutableDictionary * parameters = @{@"":@"",@"":@""};
+    
+    [manager POST:@"" parameters:parameters progress:^(NSProgress * _Nonnull uploadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+    }];
+}
+
+//创建下载任务
+-(void)downloadTask
+{
+    NSURLSessionConfiguration * configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager * manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURL * URL = [NSURL URLWithString:@"http://example.com/download.zip"];
+    NSURLRequest * request = [NSURLRequest requestWithURL:URL];
+    
+    NSURLSessionDownloadTask * downloadTask = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL * _Nonnull(NSURL * _Nonnull targetPath, NSURLResponse * _Nonnull response) {
+        NSURL * documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+        return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
+    } completionHandler:^(NSURLResponse * _Nonnull response, NSURL * _Nullable filePath, NSError * _Nullable error) {
+        NSLog(@"file downloaded to %@",filePath);
+    }];
+    [downloadTask resume];
+}
+
+//创建上传任务
+-(void)uploadTask
+{
+    NSURLSessionConfiguration * configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager * manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURL * URL = [NSURL URLWithString:@"http://example.com/upload"];
+    NSURLRequest * request = [NSURLRequest requestWithURL:URL];
+    
+    NSURL * filePath = [NSURL fileURLWithPath:@"file://path/to/image.png"];
+    NSURLSessionUploadTask * uploadTask = [manager uploadTaskWithRequest:request fromFile:filePath progress:nil completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Error : %@" , error);
+        } else {
+            NSLog(@"Success : %@ %@",response,responseObject);
+        }
+    }];
+    [uploadTask resume];
+}
+
+-(void)TaskForMultiRequest
+{
+    NSMutableURLRequest * request = [[AFHTTPRequestSerializer serializer] multipartFormRequestWithMethod:@"POST" URLString:@"http://example.com/upload" parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+        [formData appendPartWithFileURL:[NSURL fileURLWithPath:@"file://path/to/image.jpg"] name:@"file" fileName:@"filename.jpg" mimeType:@"image/jpeg" error:nil];
+    } error:nil];
+    
+    AFURLSessionManager * manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    
+    NSURLSessionUploadTask * uploadTask ;
+    uploadTask = [manager uploadTaskWithStreamedRequest:request progress:^(NSProgress * _Nonnull uploadProgress) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+        });
+    } completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"ERROR : %@", error);
+        } else {
+            NSLog(@"%@ %@",response , responseObject);
+        }
+    }];
+    [uploadTask resume];
+}
+
+-(void)dataTask
+{
+    NSURLSessionConfiguration * configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager * manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURL * URL = [NSURL URLWithString:@"http://httpbin.org/get"];
+    NSURLRequest * request = [NSURLRequest requestWithURL:URL];
+    
+    NSURLSessionDataTask * dataTask = [manager dataTaskWithRequest:request completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"ERROR: %@",error);
+        }else {
+            NSLog(@"%@ %@",response , responseObject);
+        }
+    }];
+    [dataTask resume];
+}
 @end
